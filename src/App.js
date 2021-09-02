4954d68d1d1a53a0bdcf079daa13730836c75f1a
Maksym Bychkiv <maxim.bichkiv@gmail.com>
2021-09-02 08:54:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <header>
+     <img src="https://pbs.twimg.com/profile_images/1063925348205821958/DlGcxdOl.jpg" />
+   </header>
+   
+   </>
+  );
+}
+
+export default App;
